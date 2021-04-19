9df2cf6dade6fa2b1f62146092740944e19bb28f
Rishika-05 <rishikaraj7263@gmail.com>
2021-04-19 15:13:39 +0530

    push

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 
 
 $(document).ready(function(){
-	var x=document.getElementById("sidenav");
-	var y=document.getElementById("menubar");
-	x.style.width="0px";
-	y.onclick()=function(){
-		if(x.style.width=="0px"){
-			x.style.width="230px";
+	$("#sidenav").css("width","0px");
+	$("#menubar").onclick()=function(){
+		if($("#sidenav").css("width")=="0px"){
+			$("#sidenav").css("width","230px");
 		}
 		else{
-			x.style.width="0px";
+			$("#sidenav").css("width","0px");
 		}
 	}
 })
